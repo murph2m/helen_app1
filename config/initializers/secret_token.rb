b74b9071c8fb53f0a2b0bc38d7af9a428c8eb1c8
murph2m <murph2m@gmail.com>
2011-02-18 19:29:37 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelenApp1::Application.config.secret_token = 'bcf451bffaef0049f7ecb645dffa08290edf69d961371815296e61a3b3e78cf4221692d2f35fe91f66f34b2fb1187bb243e1ed26524e37a1a92d58af68bb0166'
